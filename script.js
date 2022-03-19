8c4234a69f6e82ac969b080fd05d779bd6158749
Júlio César Mesquita Camilo Martins <94556951+devjuliomesquita@users.noreply.github.com>
2022-03-19 01:43:55 -0300

    Versao_03
    Feito o botão de login.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-//DECLARAÇÃO DA VARIÁVEL ONDE FAZER O SLIDE DE PESQUISA E CARRINHO
+//DECLARAÇÃO DA VARIÁVEL ONDE FAZER O SLIDE DE PESQUISA DO NAVBAR
 let searchForm = document.querySelector('.search-form');
 let shoppingCart = document.querySelector('.shopping-cart');
+let loginForm = document.querySelector('.login-form');
 
 //TRAZER OS ITENS PARA FRENTE
 document.querySelector('#search-btn').onclick = () =>{
@@ -9,4 +10,8 @@ document.querySelector('#search-btn').onclick = () =>{
 
 document.querySelector('#shopping-btn').onclick = () =>{
     shoppingCart.classList.toggle('active');
+};
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
 };
