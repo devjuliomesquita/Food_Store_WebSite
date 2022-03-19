12da4f0bbd40d348ca9d80511ebb97a3a9749d3c
Júlio César Mesquita Camilo Martins <94556951+devjuliomesquita@users.noreply.github.com>
2022-03-19 01:02:41 -0300

    Versao_02
    Inserção das imagens. Item de pesquisa e carrinho de compras OK.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-//DECLARAÇÃO DA VARIÁVEL ONDE FAZER O SLIDE DE PESQUISA
+//DECLARAÇÃO DA VARIÁVEL ONDE FAZER O SLIDE DE PESQUISA E CARRINHO
 let searchForm = document.querySelector('.search-form');
+let shoppingCart = document.querySelector('.shopping-cart');
 
+//TRAZER OS ITENS PARA FRENTE
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
+};
+
+document.querySelector('#shopping-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
 };
