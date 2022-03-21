2901c1303a21e630aadfbe334faab2e899810b20
Júlio César Mesquita Camilo Martins <94556951+devjuliomesquita@users.noreply.github.com>
2022-03-21 00:51:08 -0300

    Versao_09
    Finalizado a section de produtos

--- script.js
+++ script.js
@@ -40,19 +40,23 @@ window.onscroll = () => {
     loginForm.classList.remove('active');
     shoppingCart.classList.remove('active');
 }
-//script do carrossel
 var swiper = new Swiper(".produtos-slider", {
     loop:true,
     spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
     breakpoints: {
         0: {
             slidesPerView: 1,
-            },
+        },
         768: {
             slidesPerView: 2,
-            },
+        },
         1020: {
             slidesPerView: 3,
-            },
         },
-    });
+    },
+});
