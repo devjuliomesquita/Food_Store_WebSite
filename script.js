ad748dfb74fbbb5926cad64302ae5b8cd163db8c
Júlio César Mesquita Camilo Martins <94556951+devjuliomesquita@users.noreply.github.com>
2022-03-21 22:36:40 -0300

    Versao_10
    Section Review finalizada.

--- script.js
+++ script.js
@@ -40,7 +40,31 @@ window.onscroll = () => {
     loginForm.classList.remove('active');
     shoppingCart.classList.remove('active');
 }
+
+//CARROSSEL DOS PRODUTOS
 var swiper = new Swiper(".produtos-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+//CARROSSEL DOS REVIEWS
+var swiper = new Swiper(".review-slider", {
     loop:true,
     spaceBetween: 20,
     autoplay: {
